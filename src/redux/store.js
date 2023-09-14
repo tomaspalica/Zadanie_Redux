ca13b9a2a8c102624df7a6663656b989df5f8095
tomaspalica <tomasss.palica@gmail.com>
2023-09-14 11:52:48 +0200

    finished podstawy reudux

---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import { rootReducer } from "./reducer";
+
+const enchancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enchancer);
